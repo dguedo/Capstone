0a991223fd1290ed1bab03c5b018e364ca2e6a7b
dguedo <devin.guedo@gmail.com>
2016-04-20 15:50:37 -0600

    phase one

--- makeModel.R
+++ makeModel.R
@@ -2,131 +2,98 @@
 setwd("~/GitHub/Capstone")
 
 library(tm)
-library(knitr)
-library(ggplot2)
 
-## Securing the Data and Preliminary Analyses
-## Read in the files
-blog    <- readLines("data/final/en_US/en_US.blogs.txt")
-news    <- readLines("data/final/en_US/en_US.news.txt")
-#twitter <- readLines("data/final/en_US/en_US.twitter.txt")
+sampleSet <- function(){
+  ## Read in the files
+  blog    <- readLines("data/final/en_US/en_US.blogs.txt")
+  news    <- readLines("data/final/en_US/en_US.news.txt")
+  #twitter <- readLines("data/final/en_US/en_US.twitter.txt")
+  
+  ## Clean and sample the data sets
+  #set.seed(90210)
+  
+  #combined_set <- c(blog, news, twitter)
+  combined_set <- c(blog, news)
+  sample_set   <- sample(combined_set, size = 1000, replace = TRUE)
+  
+  # encode
+  sample_set <- iconv(sample_set, "latin1", "ASCII", "") 
+  
+  # create the corpus
+  swiftkey = Corpus(VectorSource(sample_set))
+  
+  # clean up objects
+  rm(blog, news, sample_set, combined_set)
+  gc()
+  
+  # clean up
+  swiftkey <- tm_map(swiftkey, content_transformer(tolower))
+  swiftkey <- tm_map(swiftkey, stripWhitespace)
+  swiftkey <- tm_map(swiftkey, removePunctuation)
+  swiftkey <- tm_map(swiftkey, removeNumbers)
+  gc()
 
-## Clean and sample the data sets
-set.seed(90210)
+  return(swiftkey)
+}
 
-#combined_set <- c(blog, news, twitter)
-combined_set <- c(blog, news)
-sample_set   <- sample(combined_set, size = 5000, replace = TRUE)
+createFrequencyMatrix <- function(sk_corpus, ngram, fileName){
+  #
+  tdm <- TermDocumentMatrix(sk_corpus, control = list(tokenize = ngram))
+  freq <- sort(rowSums(as.matrix(tdm)), decreasing = TRUE)
+  saveRDS(freq, fileName)
+  rm(tdm, freq, sk_corpus)
+  gc()
+}
 
-# encode
-sample_set <- iconv(sample_set, "latin1", "ASCII", "") 
-
-# create the corpus
-swiftkey = Corpus(VectorSource(sample_set))
-
-# clean up objects
-rm(blog, news, sample_set, combined_set)
-gc()
-
-# clean up
-swiftkey <- tm_map(swiftkey, content_transformer(tolower))
-swiftkey <- tm_map(swiftkey, stripWhitespace)
-swiftkey <- tm_map(swiftkey, removePunctuation)
-swiftkey <- tm_map(swiftkey, removeNumbers)
-
-gc()
-
-# unigram functions
 # use unigram for tie breaks
 # create a simple top 3 words list? as last resort
-unigram <- function(x) unlist(lapply(ngrams(words(x), 1), paste, collapse = " "), use.names = FALSE)
-bigram  <- function(x) unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
-trigram <- function(x) unlist(lapply(ngrams(words(x), 3), paste, collapse = " "), use.names = FALSE)
+
+# n-gram functions
+unigram  <- function(x) unlist(lapply(ngrams(words(x), 1), paste, collapse = " "), use.names = FALSE)
+bigram   <- function(x) unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
+trigram  <- function(x) unlist(lapply(ngrams(words(x), 3), paste, collapse = " "), use.names = FALSE)
 fourgram <- function(x) unlist(lapply(ngrams(words(x), 4), paste, collapse = " "), use.names = FALSE)
 
-tdm3 <- TermDocumentMatrix(swiftkey, control = list(tokenize = trigram))
-freq3 <- sort(rowSums(as.matrix(tdm3)), decreasing = TRUE)
-saveRDS(freq3, "data/trigram.rds")
-#rm(tdm3, freq3)
-#gc()
+# save to disk
+createFrequencyMatrix(sampleSet(), unigram, "data/unigram.rds")
+createFrequencyMatrix(sampleSet(), bigram, "data/bigram.rds")
+createFrequencyMatrix(sampleSet(), trigram, "data/trigram.rds")
+createFrequencyMatrix(sampleSet(), fourgram, "data/fourgram.rds")
 
-#tdm1 <- TermDocumentMatrix(swiftkey, control = list(tokenize = unigram))
-#tdm2 <- TermDocumentMatrix(swiftkey, control = list(tokenize = bigram))
+# final model
+finalModel <- function(file_freq, n){
+  
+  fm <- data.frame(readRDS(file_freq))
+  names(fm)[1] <- "freq"
+  fm$phrase <- row.names(fm)
+  
+  fm$first  <- lapply(fm$phrase, function(x) strsplit(x,split=" ")[[1]][1])
+  if (n == 2){
+    fm$second  <- lapply(fm$phrase, function(x) strsplit(x,split=" ")[[1]][2])
+  } else if (n == 3){
+    fm$second  <- lapply(fm$phrase, function(x) strsplit(x,split=" ")[[1]][2])
+    fm$third  <- lapply(fm$phrase, function(x) strsplit(x,split=" ")[[1]][3])
+  } else if (n == 4){
+    fm$second  <- lapply(fm$phrase, function(x) strsplit(x,split=" ")[[1]][2])
+    fm$third  <- lapply(fm$phrase, function(x) strsplit(x,split=" ")[[1]][3])
+    fm$fourth  <- lapply(fm$phrase, function(x) strsplit(x,split=" ")[[1]][4])
+  }
 
-#rm(tdm2)
-#gc()
+  return(fm)
+}
 
-#rm(swiftkey)
-#gc()
+ngram.uni  <- finalModel("data/unigram.rds", 1)
+ngram.bi   <- finalModel("data/bigram.rds", 2)
+ngram.tri  <- finalModel("data/trigram.rds", 3)
+ngram.four <- finalModel("data/fourgram.rds", 4)
 
-#tdm3Sparse <- removeSparseTerms(tdm3, .999)
-#rm(tdm3)
-#gc()
+save(ngram.uni, ngram.bi, ngram.tri, ngram.four, file="pridictionModel.rda")
 
-#tdm3Sparse
+#
+load("pridictionModel.rda")
+x <- ngram.tri[which(ngram.tri$first == "a" & ngram.tri$second == "couple"), ]
+x[,c(1,5)]
 
-#freq2 <- sort(rowSums(as.matrix(tdm2Sparse)), decreasing = TRUE)
-
-#freq2 <- sort(rowSums(as.matrix(tdm2Sparse)), decreasing = TRUE)
-
-#rm(swiftkey)
-
-#tdm3 <- TermDocumentMatrix(swiftkey, control = list(tokenize = trigram))
-#freq3 <- sort(rowSums(as.matrix(tdm3)), decreasing = TRUE)
-
-# cleanup
-#rm(swiftkey, tdm3)
-#gc()
-
-#freq1 <- sort(rowSums(as.matrix(tdm1)), decreasing = TRUE)[1:15]
-#rm(tdm1)
-
-#freq2 <- sort(rowSums(as.matrix(tdm2)), decreasing = TRUE)[1:15]
-#rm(tdm2)
-
-#freq3 <- sort(rowSums(as.matrix(tdm3)), decreasing = TRUE)[1:15]
-#rm(tdm3)
-
-#t1 <- freq3[grep("i+", names(freq3))]
-
-#freq3[grep("i+", names(freq3))][1]
-#saveRDS(freq3, "mymodel.rds")
-
-#mod2 <- readRDS("mymodel.rds")
-
-
-#########################################
-#str(freq3)
-
-#tt <- data.frame(freq3)
-
-#tt$phrase <- row.names(tt)
-
-#tt$first  <- lapply(tt$phrase, function(x) strsplit(x,split=" ")[[1]][1] )
-#tt$second <- lapply(tt$phrase, function(x) strsplit(x,split=" ")[[1]][2] )
-#tt$third  <- lapply(tt$phrase, function(x) strsplit(x,split=" ")[[1]][3] )
-
-#tt[4,]
-
-#x <- tt[which(tt$first == "a" & tt$second == "couple"), ]
-
-#names(tt)[names(tt)=="freq3"] <- "freq"
-
-#tt$phrase <- NULL
-
-#ngram.tri <- tt
-#ngram.bi <- tt
-
-#save(ngram.tri, ngram.bi, file="pridictionModel.rda")
-
-#rm(finalModel.bigram, finalModel.trigram)
-
-#load("pridictionModel.rda")
-
-
-
-#saveRDS(finalModel.trigram, "pridictionModel.rds")
-
-#mod2 <- readRDS("pridictionModel.rds")
-
+rm(ngram.uni, ngram.bi, ngram.tri, ngram.four)
+gc()
 
